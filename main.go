497a5e270fd77001e5f38bba60e543db8d3eca10
Sebastian Hogas <sehogas@gmail.com>
2025-03-23 23:08:36 -0300

    test QR_GENERATOR_PING_RESPONSE

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func main() {
 
 	var PING_RESPONSE = os.Getenv("QR_GENERATOR_PING_RESPONSE")
 	if PING_RESPONSE == "" {
-		PING_RESPONSE = "pongooo"
+		PING_RESPONSE = "pong"
 	}
 
 	http.HandleFunc("/", QrGeneratorHandler)
